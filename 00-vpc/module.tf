f43fcfe50074ddac97e7194d3058a915b4188fb8
asif <asifalishaik415@gmail.com>
2026-03-13 18:29:40 -0700

    terraform

--- 00-vpc/module.tf
+++ 00-vpc/module.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-    source = "C:/devops/daws88s/repos\terraform-aws-vpc"
+    source = "git::https://github.com/asifali-shaik/roboshop-infra-dev.git?ref=main"
     project = var.project
     envrionment = var.enrionment
     is_peering_required = true
